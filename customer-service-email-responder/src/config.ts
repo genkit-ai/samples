f0c28a8fc7c921e8dc34d3146f42ca0649a7448a
huangjeff5 <64040981+huangjeff5@users.noreply.github.com>
2025-02-27 10:33:49 -0600

    Update config.ts

--- customer-service-email-responder/src/config.ts
+++ customer-service-email-responder/src/config.ts
@@ -6,11 +6,6 @@ import genkitEval, { GenkitMetric } from '@genkit-ai/evaluator';
 export const ai = genkit({
   plugins: [
     vertexAI({projectId: process.env.PROJECT_ID, location: process.env.LOCATION || "us-central1"}),
-    genkitEval({
-      judge: gemini15Pro,
-      metrics: [GenkitMetric.FAITHFULNESS, GenkitMetric.ANSWER_RELEVANCY],
-      embedder: textEmbedding004,
-    }),
   ],
   model: gemini15Flash
 });
