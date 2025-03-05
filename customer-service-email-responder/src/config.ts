96775f6af371acf328762de8edcc07d949091472
Jeff Huang <huangjeff@google.com>
2025-03-05 12:35:22 -0600

    update sample to use google ai

--- customer-service-email-responder/src/config.ts
+++ customer-service-email-responder/src/config.ts
@@ -1,17 +1,9 @@
 import { genkit } from 'genkit';
-import vertexAI, { gemini10Pro, gemini15Flash, gemini15Pro, textEmbedding004 } from '@genkit-ai/vertexai';
-import genkitEval, { GenkitMetric } from '@genkit-ai/evaluator';
+import { gemini20Flash, googleAI } from '@genkit-ai/googleai';
 
 // Configure Genkit with necessary plugins
 export const ai = genkit({
-  plugins: [
-    vertexAI({projectId: process.env.PROJECT_ID, location: process.env.LOCATION || "us-central1"}),
-    genkitEval({
-      judge: gemini15Pro,
-      metrics: [GenkitMetric.FAITHFULNESS, GenkitMetric.ANSWER_RELEVANCY],
-      embedder: textEmbedding004,
-    }),
-  ],
-  model: gemini15Flash
+  plugins: [googleAI()],
+  model: gemini20Flash
 });
 
