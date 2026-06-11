8ae3f3609673096fd0adec8767f0f747ca16752f
Alex Duan <alexduan@google.com>
2026-06-11 14:55:27 -0700

    refactor: Move server index.js to src/folder

--- agent-streaming/src/server/index.js
+++ agent-streaming/src/server/index.js
@@ -96,7 +96,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 // API Endpoint to stream the chat response (Server-Sent Events)
 app.post('/api/chat', async (req, res) => {
